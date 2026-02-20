bb3292dc4c7c02adc3914177b81682b28747a663
sapronov81-ctrl <sapronov81@gmail.com>
2026-02-21 02:03:30 +0300

    Create subscription endpoints file

---
+++ app/api/subscriptions/route.ts
@@ -0,0 +1,17 @@
+// Subscription endpoints
+
+export const getSubscriptions = (req, res) => {
+    // Logic to fetch subscriptions
+};
+
+export const createSubscription = (req, res) => {
+    // Logic to create a new subscription
+};
+
+export const updateSubscription = (req, res) => {
+    // Logic to update an existing subscription
+};
+
+export const deleteSubscription = (req, res) => {
+    // Logic to delete a subscription
+};
